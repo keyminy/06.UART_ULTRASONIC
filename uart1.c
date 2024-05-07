dee43f19d7a0852b86c2af05bd3b04c71b06ece4
keyminy <imanaifs@gmail.com>
2024-05-07 17:41:22 +0900

    오실로 과제

--- uart1.c
+++ uart1.c
@@ -63,7 +63,9 @@ void bit_command_processing()
 {
 	#if 1 // 이 code는 test용입니다. 사용후 반드시 삭제할 것.
 		// 100ms마다 1개씩 데이터를 쏜다고 해봅시다.
-		UART1_transmit('a'); //'a' = 0x61(아스키), 0110_0001
+		UART1_transmit('_'); //'_' =95(10진) = 0x5F = 0101_1111
+		_delay_ms(500);
+		UART1_transmit('W'); //'W' =87(10진)= 0x57 = 0101_0111
 		_delay_ms(500);
 	#endif
 	char* cmd;
